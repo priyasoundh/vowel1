0002f7134955cb35ba42991555f42eddbf7a5e10
priyadharshiniap <32993324+priyadharshiniap@users.noreply.github.com>
2017-12-21 09:45:17 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+char a;
+cout<<"enter a letter";
+cin>>a;
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+  cout<<"vowel";
+}
+else
+{
+  cout<<"consonant";
+}
+getch();
+}
